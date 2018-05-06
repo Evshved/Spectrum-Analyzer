35065b4c7fc90febc5b573d4d4a80e83fb6fe885
roman412 <romanio412@gmail.com>
2018-05-06 23:57:33 +0300

    Haven't added file to previous commit.

---
+++ SpectrumAnalyzer/Helpers/SpectrumProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectrumAnalyzer.Helpers
+{
+    public class SpectrumProfile
+    {
+        internal Spectrum OriginalData;
+        internal Dictionary<string, Spectrum> Transitions = new Dictionary<string, Spectrum>();
+
+        internal SpectrumProfile(Spectrum spectrum)
+        {
+            OriginalData = spectrum;
+        }
+    }
+}
