047859a45b40b18bcbccb0a2ef86c65625c42b75
roman412 <romanio412@gmail.com>
2018-05-18 03:19:57 +0300

    Most of markup design was moved to Resources dictionary. Added localization resources library. Starting window size is being set from screen size as 0.6 of original height and 0.5 of original width.

---
+++ SpectrumAnalyzer/Helpers/WindowSizeConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SpectrumAnalyzer.Helpers
+{
+    public class WindowSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double size = System.Convert.ToDouble(value) * System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
+            return size.ToString("G0", CultureInfo.InvariantCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => DependencyProperty.UnsetValue;
+    }
+}
