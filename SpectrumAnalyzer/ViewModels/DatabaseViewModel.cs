8707d32ce97bf26a5027dbe988aa34f61048c89f
roman412 <romanio412@gmail.com>
2018-07-27 19:44:47 +0300

    Implement option to delete spectrums from database.

--- SpectrumAnalyzer/ViewModels/DatabaseViewModel.cs
+++ SpectrumAnalyzer/ViewModels/DatabaseViewModel.cs
@@ -7,12 +7,14 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace SpectrumAnalyzer.ViewModels
 {
     public class DatabaseViewModel : Screen
     {
         public BindableCollection<Spectrums> Spectrums { get; set; } = new BindableCollection<Spectrums>();
+        public List<Spectrums> SelectedSpectrums { get; set; } = new List<Spectrums>();
 
         public DatabaseViewModel()
         {
@@ -26,6 +28,29 @@ namespace SpectrumAnalyzer.ViewModels
             {
                 Spectrums.Add(spectrum);
             }
+        }
+
+        public void DeleteSpectrums()
+        {
+            var spectrumsToRemove = SelectedSpectrums;
+            foreach (var spectrum in spectrumsToRemove)
+            {
+                Database.GetConnection().Delete<Spectrum>(spectrum.ID);
+                SelectedSpectrums.Remove(spectrum);
+                Spectrums.Remove(spectrum);
+            }
+        }
+
+        public bool CanDeleteSpectrums
+        {
+            get { return SelectedSpectrums.Count > 0 ? true : false; }
+        }
+
+        public void Spectrums_SelectionChanged(SelectionChangedEventArgs args)
+        {
+            SelectedSpectrums.AddRange(args.AddedItems.Cast<object>().Where(x => x is Spectrums).Cast<Spectrums>());
+            args.RemovedItems.Cast<object>().Where(x => x is Spectrums).Cast<Spectrums>().ToList().ForEach(x => SelectedSpectrums.Remove(x));
+            NotifyOfPropertyChange(() => CanDeleteSpectrums);
         }
     }
 }
